8a0f7f451b0e3a651361253f6464088f052458af
Luciano Morais <ljimorais@gmail.com>
2023-11-01 11:25:41 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/cursojava/curso/entities/User.java
@@ -0,0 +1,90 @@
+package com.cursojava.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private String emial;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public User(long id, String nome, String emial, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.emial = emial;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmial() {
+		return emial;
+	}
+
+	public void setEmial(String emial) {
+		this.emial = emial;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+	
+
+	
+	
+}
